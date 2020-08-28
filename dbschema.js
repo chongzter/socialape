82d053e722462f5ed9f83e0de698b1baaa6a712e
zackster <ftchong235@yahoo.com>
2020-08-28 18:22:04 +0800

    #3 Express & formatting response

---
+++ dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+  screams: [
+    {
+      userHandle: "user",
+      body: "this is the scream body",
+      createdAt: "2020-08-28T10:11:36.088Z",
+      likeCount: 5,
+      commentCount: 2,
+    },
+  ],
+};
