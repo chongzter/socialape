ae22b138ae5fba9ff48c8e2fd943eff355bb5744
zackster <ftchong235@yahoo.com>
2020-09-02 19:19:53 +0800

    #7 Refactoring & Organising

---
+++ functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apiKey: "AIzaSyDhh7p-WbwrjAff-iXKTt--38HmJCGlLXY",
+  authDomain: "socialape-a7335.firebaseapp.com",
+  databaseURL: "https://socialape-a7335.firebaseio.com",
+  projectId: "socialape-a7335",
+  storageBucket: "socialape-a7335.appspot.com",
+  messagingSenderId: "7061776119",
+  appId: "1:7061776119:web:8e4498477a8d9abc0495f2",
+  measurementId: "G-YM0NPE0F37",
+};
