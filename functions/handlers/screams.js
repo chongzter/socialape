83a8369d998abb9ba4c2f8e528d12a984f4176a1
zackster <ftchong235@yahoo.com>
2020-09-10 14:43:40 +0800

    #13 Finishing up Cloud Functions

--- functions/handlers/screams.js
+++ functions/handlers/screams.js
@@ -12,6 +12,7 @@ exports.getAllScreams = (req, res) => {
           body: doc.data().body,
           userHandle: doc.data().userHandle,
           createdAt: doc.data().createdAt,
+          userImage: doc.data().userImage,
         });
       });
       return res.json(screams);
@@ -78,7 +79,7 @@ exports.getScream = (req, res) => {
 // Comment a comment
 exports.commentOnScream = (req, res) => {
   if (req.body.body.trim() === "")
-    return res.status(400).json({ error: "Must not be empty" });
+    return res.status(400).json({ comment: "Must not be empty" });
 
   const newComment = {
     body: req.body.body,
